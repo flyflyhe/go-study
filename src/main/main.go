e8c0f7ec7d82135700eba03fe7a9f03216ede314
mrHe <hejixnue001@163.com>
2017-10-18 17:44:04 +0800

    http

--- src/main/main.go
+++ src/main/main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-	"services/trusted"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+	"time"
 )
 
+type Handle struct {
+}
+
+func (e Handle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       //解析参数，默认是不会解析的
+	fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
+}
+
 func main() {
-	trusted.Run()
+	handle := &Handle{}
+	s := &http.Server{
+		Addr:           ":9090",
+		Handler:        handle,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
 }
